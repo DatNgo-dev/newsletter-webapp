1dd4dcec3e11604d62b9064f723cf10354d8bf3a
Dat Ngo <dat_ngo10@protonmail.com>
2022-12-02 18:57:11 +0800

    Init: Initialise a vite react typescript newsletter project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-red-400 text-black">Hello World</p>
+    </div>
+  )
+}
+
+export default App
